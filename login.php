f117735f6035e9aecb83b67b63b65d3248fe7e37
Fabien Marie <fmarie.houeix@gmail.com>
2015-10-14 15:29:28 +0200

    Modification login

--- login.php
+++ login.php
@@ -11,18 +11,25 @@ require('model/functions.fn.php');
 			PROCESS
 ********************************/
 
-if(isset($_POST['email']) && isset($_POST['password'])){
-	if(!empty($_POST['email']) && !empty($_POST['password'])){
+if (isset($_POST['email']) && !empty($_POST['email']) &&
+	isset($_POST['password']) && !empty($_POST['password'])
+) {
+	$email = $_POST['email'];
+	$passeword = $_POST['password'];
 
-		// TODO
-
-		// Force user connection to access dashboard
-		userConnection($db, 'git@initiation.com', 'password');
-		
+	/*userConnection
+		return :
+			true for connection OK
+			false for fail
+		$db -> 				database object
+		$email -> 			field value : email
+		$password -> 		field value : password
+	*/
+	if (userConnection($db, $email, $passeword)) {
 		header('Location: dashboard.php');
-
-	}else{
-		$error = 'Champs requis !';
+	}
+	else {
+		$error = "Mauvaise identification";
 	}
 }
 
